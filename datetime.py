81458191ffdc86d5915241c5d618b3f3fb60e670
KEYUR0609 <joshikeyur423@gmail.com>
2026-04-21 11:25:03 +0530

    Create datetime.py

---
+++ datetime.py
@@ -0,0 +1,15 @@
+import datetime
+
+# Current Date
+today = datetime.date.today()
+print("Today's Date:", today)
+
+# Current Time
+now = datetime.datetime.now()
+print("Current Date & Time:", now)
+
+# Only Time
+print("Current Time:", now.time())
+
+# Access individual parts
+print("Year
