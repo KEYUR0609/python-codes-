b836f99a9882dc6bc8e555987d6f4bcf0c0f71f5
KEYUR0609 <joshikeyur423@gmail.com>
2026-04-21 11:28:21 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,5 +1,36 @@
-def multiply(a, b):
+def add(a, b):
+    print("Addition =", a + b)
+
+def sub(a, b):
+    print("Subtraction =", a - b)
+
+def mul(a, b):
     print("Multiplication =", a * b)
 
-if __name__ == "__main__":
-    multiply(2, 3)
+def div(a, b):
+    if b != 0:
+        print("Division =", a / b)
+    else:
+        print("Division not possible")
+
+# Main Program
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+choice = int(input("Enter your choice: "))
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+if choice == 1:
+    add(x, y)
+elif choice == 2:
+    sub(x, y)
+elif choice == 3:
+    mul(x, y)
+elif choice == 4:
+    div(x, y)
+else:
+    print("Invalid choice")
